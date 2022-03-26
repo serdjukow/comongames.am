43d13ce4a90e2d691c60f8032ebee715d3f19a5a
serdjukow <sergej@serdjukow.eu>
2022-03-26 18:00:25 +0100

    U-4

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
-const scrollToElement = () => {
-	document.querySelectorAll('a[href^="#"]').forEach(link => {
-		link.addEventListener('click', e => {
-			e.preventDefault()
-			const section = e.target.getAttribute('href').substring(1)
-			if (section) {
-				const y = document.getElementById(`${section}`).getBoundingClientRect().top + window.pageYOffset
-				seamless.scrollTo(window, { top: y, behavior: 'smooth' })
-			}
+document.addEventListener('DOMContentLoaded', () => {
+	const scrollToElement = () => {
+		document.querySelectorAll('a[href^="#"]').forEach(link => {
+			link.addEventListener('click', e => {
+				e.preventDefault()
+				const section = e.target.getAttribute('href').substring(1)
+				if (section) {
+					const y = document.getElementById(`${section}`).getBoundingClientRect().top + window.pageYOffset
+					seamless.scrollTo(window, { top: y, behavior: 'smooth' })
+				}
+			})
 		})
-	})
-}
-scrollToElement()
+	}
+	scrollToElement()
 
-function showInfoMessage(message, status) {
-	const infoMessages = document.querySelector('.info-message')
-	const infoMessagesBody = document.querySelector('.info-message__body')
+	function showInfoMessage(message, status) {
+		const infoMessages = document.querySelector('.info-message')
+		const infoMessagesBody = document.querySelector('.info-message__body')
 
-	const ok = 'images/ok.svg'
-	const error = 'images/error.svg'
+		const ok = 'images/ok.svg'
+		const error = 'images/error.svg'
 
-	infoMessages.classList.add('show')
-	infoMessagesBody.innerHTML = `
+		infoMessages.classList.add('show')
+		infoMessagesBody.innerHTML = `
 	<div class="info-messages__img">
 		<div class="info-message__img-container">
 			<img src="${status === 'ok' ? ok : error}" alt="${message}">
@@ -31,49 +32,48 @@ function showInfoMessage(message, status) {
 		</div>
 	`
 
-	setTimeout(() => {
-		infoMessages.classList.remove('show')
-		infoMessagesBody.textContent = ''
-	}, 3000)
-}
+		setTimeout(() => {
+			infoMessages.classList.remove('show')
+			infoMessagesBody.textContent = ''
+		}, 3000)
+	}
 
-//showInfoMessage(`<p>Thank you for choosing us!</p><p>Your request has been accepted, our manager will contact you soon.</p>`, 'ok')
+	//showInfoMessage(`<p>Thank you for choosing us!</p><p>Your request has been accepted, our manager will contact you soon.</p>`, 'ok')
 
-gsap.registerPlugin(ScrollTrigger)
+	gsap.registerPlugin(ScrollTrigger)
 
-const headerLogo = document.querySelector('.header__logo')
-
-if (headerLogo) {
-	gsap.from(headerLogo, {
-		scrollTrigger: {
-			trigger:headerLogo,
-			toggleActions: 'restart pause restart pause',
-		},
-		opacity: 0,
-		duration: 1
-	})
-}
-
-const pageWidth = document.documentElement.scrollWidth
-const gameBlocks = document.querySelectorAll('.game')
-
-if (pageWidth > 768) {
-	gameBlocks.forEach(block => {
-		gsap.from(block.querySelector('.game__icon'), {
+	if (document.querySelector('.header__logo')) {
+		gsap.from(document.querySelector('.header__logo'), {
+			duration: 1,
+			scale: 0.7,
 			scrollTrigger: {
-				trigger: block.querySelector('.game__icon'),
+				trigger: document.querySelector('.header__logo'),
 				toggleActions: 'restart pause restart pause',
 			},
-			x: -30,
-			duration: 1,
-		}),
-			gsap.from(block.querySelector('.game__content'), {
+		})
+	}
+
+	const pageWidth = document.documentElement.scrollWidth
+	const gameBlocks = document.querySelectorAll('.game')
+
+	if (pageWidth > 768) {
+		gameBlocks.forEach(block => {
+			gsap.from(block.querySelector('.game__icon'), {
 				scrollTrigger: {
-					trigger: block.querySelector('.game__content'),
+					trigger: block.querySelector('.game__icon'),
 					toggleActions: 'restart pause restart pause',
 				},
-				x: 30,
-				duration: 1
-			})
-	})
-}
+				duration: 1,
+				x: -30,
+			}),
+				gsap.from(block.querySelector('.game__content'), {
+					scrollTrigger: {
+						trigger: block.querySelector('.game__content'),
+						toggleActions: 'restart pause restart pause',
+					},
+					duration: 1,
+					x: 30
+				})
+		})
+	}
+})
